a2cf44fbe1cd5738ddd0b907b29932639699bd55
Kseniya Dergunova <kdergunova@vocalize.ru>
2020-05-21 14:29:33 +0300

    Синхронизация общих ресурсов.[#268046]

---
+++ chapter_001/src/main/java/ru/job4j/concurrent/Cache.java
@@ -0,0 +1,18 @@
+package ru.job4j.concurrent;
+
+/**
+ * Class Cache
+ *
+ * @author Kseniya Dergunova
+ * @since 21.05.2020
+ */
+public class Cache {
+    private static Cache cache;
+
+    private synchronized static Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
