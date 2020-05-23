9e76e10c204aef1bb0576c891769b256ec2a819a
Kseniya Dergunova <kdergunova@vocalize.ru>
2020-05-23 16:19:19 +0300

    класс Count и метод int increment()[#268049]

---
+++ chapter_001/src/main/java/ru/job4j/concurrent/ParseFile.java
@@ -0,0 +1,51 @@
+package ru.job4j.concurrent;
+
+import java.io.*;
+
+/**
+ * Class ParseFile
+ *
+ * @author Kseniya Dergunova
+ * @since 23.05.2020
+ */
+public class ParseFile {
+    private File file;
+
+    public synchronized void setFile(File f) {
+        file = f;
+    }
+
+    public synchronized File getFile() {
+        return file;
+    }
+
+    public synchronized String getContent() throws IOException {
+        try (InputStream i = new FileInputStream(file)) {
+            StringBuilder text = new StringBuilder();
+            int data;
+            while ((data = i.read()) != -1) {
+                text.append((char) data);
+            }
+            return text.toString();
+        }
+    }
+
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        try (InputStream i = new FileInputStream(file)) {
+            StringBuilder text = new StringBuilder();
+            int data;
+            while ((data = i.read()) > 0) {
+                if (data < 0x80) {
+                    text.append((char) data);
+                }
+            }
+            return text.toString();
+        }
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        try (OutputStream o = new FileOutputStream(file)) {
+            o.write(content.getBytes());
+        }
+    }
+}
