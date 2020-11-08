f0fa3039afac0f4745437c292c116fd31b88ee12
Kseniya Dergunova <kdergunova@vocalize.ru>
2020-11-08 14:08:56 +0300

    4. Thread без общих ресурсов[267919#268045]

---
+++ chapter_001/src/main/java/ru/job4j/concurrent/immutable/User.java
@@ -0,0 +1,34 @@
+package ru.job4j.concurrent.immutable;
+
+/**
+ * Class User
+ *
+ * @author Kseniya Dergunova
+ * @since 08.11.2020
+ */
+public class User {
+    private int id;
+    private String name;
+
+    public static User of(String name) {
+        User user = new User();
+        user.name = name;
+        return user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
