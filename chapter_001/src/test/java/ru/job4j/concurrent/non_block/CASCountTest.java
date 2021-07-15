244b400d4301946b3cba27d42fc777643733d415
Kseniya Dergunova <kdergunova@vocalize.ru>
2021-07-15 17:43:13 +0300

    Проверка регулярки.

--- chapter_001/src/test/java/ru/job4j/concurrent/non_block/CASCountTest.java
+++ chapter_001/src/test/java/ru/job4j/concurrent/non_block/CASCountTest.java
@@ -3,6 +3,9 @@ package ru.job4j.concurrent.non_block;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.hamcrest.Matchers.is;
 
 class CASCountTest {
@@ -29,5 +32,18 @@ class CASCountTest {
         count.increment();
         count.increment();
         MatcherAssert.assertThat(count.get(), is(3));
+    }
+
+    @Test
+    void replace() {
+        String regex = "[0-9]";
+        String phone = "+7 (904) 497-97-20";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phone);
+        String res = "";
+        while (matcher.find()) {
+            res = res.concat(phone.substring(matcher.start(), matcher.end()));
+        }
+        System.out.println(res);
     }
 }
